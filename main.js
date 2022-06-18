4fa027c7e7656c0b3bbc2617a0d6a6292dbbd94c
Gerardo J Garcia <gerardojgarcia@pm.me>
2022-06-17 17:48:58 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const navItemEl = document.querySelectorAll('.nav-item');
+const hamBtn = document.getElementById('ham-btn')
+
+const mobileMenu = document.querySelector('mobile-menu')
+
+
+
+hamBtn.addEventListener('click', () => {
+    console.log('i was clicked')
+})
